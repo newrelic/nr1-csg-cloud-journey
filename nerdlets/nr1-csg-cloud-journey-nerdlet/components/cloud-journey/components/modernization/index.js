9a22d45aabb4cc3ebb273f03e854c8f5e70933ad
ricegi <gil@newrelic.com>
2019-11-12 07:37:12 -0500

    update to modernization dependencies

--- nerdlets/nr1-csg-cloud-journey-nerdlet/components/cloud-journey/components/modernization/index.js
+++ nerdlets/nr1-csg-cloud-journey-nerdlet/components/cloud-journey/components/modernization/index.js
@@ -27,7 +27,7 @@ import { NerdletStateContext } from 'nr1';
  * Process Modernization: https://one.newrelic.com/launcher/dashboards.launcher?packages=local#launcher=eyJ0aW1lUmFuZ2UiOnsiYmVnaW5fdGltZSI6bnVsbCwiZW5kX3RpbWUiOm51bGwsImR1cmF0aW9uIjpudWxsfX0=&pane=eyJuZXJkbGV0SWQiOiJkYXNoYm9hcmRzLmRhc2hib2FyZCIsImVudGl0eUlkIjoiTWpJeU56azBPWHhXU1ZwOFJFRlRTRUpQUVZKRWZEZ3lNakV6TlEiLCJpc1RlbXBsYXRlRW1wdHkiOmZhbHNlfQ==&state=IkU4MjNCRjIzLTQ2NEQtQ0U0NS04NTBCLTg5MUM5OURENTlDNyI=
  * Cloud Native Services: https://one.newrelic.com/launcher/dashboards.launcher?packages=local#launcher=eyJ0aW1lUmFuZ2UiOnsiYmVnaW5fdGltZSI6bnVsbCwiZW5kX3RpbWUiOm51bGwsImR1cmF0aW9uIjpudWxsfX0=&pane=eyJuZXJkbGV0SWQiOiJkYXNoYm9hcmRzLmRhc2hib2FyZCIsImVudGl0eUlkIjoiTVRZd05qZzJNbnhXU1ZwOFJFRlRTRUpQUVZKRWZEazFPVEkyT1EiLCJpc1RlbXBsYXRlRW1wdHkiOmZhbHNlfQ==
  * Infrastructure Modernization: K8 Cluster View
- * Modernization Patterns Nerdlet: https://newrelic.jiveon.com/people/bthomason@newrelic.com/blog/2019/11/01/introducing-the-cas-modernization-nerdpack
+ * Modernization Patterns Nerdlet: https://newrelic.jiveon.com/people/bthomason@newrelic.com/blog/2019/11/01/introducing-the-cas-modernization-nerdpack (https://source.datanerd.us/bthomason/modernization-nerdpack)
  */
 export default class Modernization extends Component {
 
